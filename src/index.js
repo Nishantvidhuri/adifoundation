f12aa3ddbbb680297f4cccde191ae8fe4239ba4b
Nishantvidhuri <nishantvidhuri0987@gmail.com>
2024-10-22 12:38:29 +0530

    update  package.json    src/index.js

--- src/index.js
+++ src/index.js
@@ -1,7 +1,10 @@
+/**
+ * Entry point of application, where App is rendered within the div with the id of "app" 
+ */
+
 import React from "react";
-import { createRoot } from "react-dom/client";
+import { render } from "react-dom";
+
 import App from "./App";
 
-const rootElement = document.getElementById("app");
-const root = createRoot(rootElement);
-root.render(<App />);
+render(<App></App>, document.getElementById("app"));
